9936ab409e2c2278103d945642482916a7f5dcb6
corprt <143188080+corprt@users.noreply.github.com>
2023-09-22 14:00:54 +0530

    changes

--- app.py
+++ app.py
@@ -1848,7 +1848,7 @@ elif selected_option_case_type == "AML":
                                             do not include any other unnecessary information or the Balance amount.\n\n
                             
                                             Context: {context_1}\n\
-                                            Response: (Give me a concise and short response in few words)'''
+                                            Response: (Give me a concise and short response in few words.Do not write anythin out of the context.)'''
 
                                 response = usellm(prompt_1)
                                 chat_history_1[query] = response
@@ -1860,7 +1860,7 @@ elif selected_option_case_type == "AML":
                                             rapid movement of funds, transactions with high-risk countries, or unexplained source of funds. Specifically, all transactions above or \ 
                                             equal to $10,000 are considered to be a potential money laundering transaction. Answer the question considering the factors mentioned above with transaction details.\n\n\
                                             Context: {context_1}\n\
-                                            Response: (Give me a concise and short response in few words)'''
+                                            Response: (Give me a concise and short response.)'''
 
                                 response = usellm(prompt_1)
                                 chat_history_1[query] = response
@@ -1872,7 +1872,7 @@ elif selected_option_case_type == "AML":
                                             patterns observed. The type may include Layering, Structuring, Round-tripping etc. \
                                             Look carefully into the transactions statement and give a precise answer with explanation of why you think a specific type of money laundering is happening..\n\n\
                                             Context: {context_1}\n\
-                                            Response:(Give me a concise and short response in few words)'''
+                                            Response:(Give me a concise and short response.)'''
 
                                 response = usellm(prompt_1)
                                 chat_history_1[query] = response
