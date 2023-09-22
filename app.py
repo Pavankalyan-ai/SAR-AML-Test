3dad14ef35c1a6c450240b3d2f4f680cbab6ca0d
corprt <143188080+corprt@users.noreply.github.com>
2023-09-22 21:22:52 +0530

    changes

--- app.py
+++ app.py
@@ -760,25 +760,25 @@ elif selected_option_case_type == "Fraud transaction dispute":
 
             with col2_up:
                 # Creating header
-                col1,col2 = st.columns(2)
-                with col1:
-                    st.markdown("""<span style="font-size: 24px; ">Pre-Set Questionnaire</span>""", unsafe_allow_html=True)
-                    # Create a Pandas DataFrame with your data
-                    data = {'Questions': [" What is the victim's name?","What is the suspect's name?",' List the merchant name',' How was the bank notified?',' When was the bank notified?',' What is the fraud type?',' When did the fraud occur?',' Was the disputed amount greater than 5000 USD?',' What type of cards are involved?',' Was the police report filed?']}
-                    df_fixed = pd.DataFrame(data)
-                    df_fixed.index = df_fixed.index +1
-                with col2:
-                    # Create a checkbox to show/hide the table
-                    cols1, cols2, cols3, cols4 = st.columns([1,1,1,1])
-                    with cols1:
-                        show_table1 = st.toggle(label="", 
-                                            key="Key1", 
-                                            default_value=False, 
-                                            label_after = False, 
-                                            inactive_color = '#D3D3D3', 
-                                            active_color="#11567f", 
-                                            track_color="#29B5E8"
-                                            )
+                # col1,col2 = st.columns(2)
+                # with col1:
+                st.markdown("""<span style="font-size: 24px; ">Pre-Set Questionnaire</span>""", unsafe_allow_html=True)
+                # Create a Pandas DataFrame with your data
+                data = {'Questions': [" What is the victim's name?","What is the suspect's name?",' List the merchant name',' How was the bank notified?',' When was the bank notified?',' What is the fraud type?',' When did the fraud occur?',' Was the disputed amount greater than 5000 USD?',' What type of cards are involved?',' Was the police report filed?']}
+                df_fixed = pd.DataFrame(data)
+                df_fixed.index = df_fixed.index +1
+                # with col2:
+                # Create a checkbox to show/hide the table
+                # cols1, cols2, cols3, cols4 = st.columns([1,1,1,1])
+                # with cols1:
+                show_table1 = tog.st_toggle(label="", 
+                                    key="Key1", 
+                                    default_value=False, 
+                                    label_after = False, 
+                                    inactive_color = '#D3D3D3', 
+                                    active_color="#11567f", 
+                                    track_color="#29B5E8"
+                                    )
                     # Show the table if the checkbox is ticked
                     if show_table1:
                         df_fixed["S.No."] = df_fixed.index
