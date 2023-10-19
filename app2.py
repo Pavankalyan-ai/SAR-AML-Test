0ef40907029b027c92027c91002d5eff1df011cf
prince aggarwal <prince183378@exlservice.com>
2023-10-19 13:15:58 +0530

    New commit

--- app2.py
+++ app2.py
@@ -1922,7 +1922,7 @@ elif selected_option_case_type == "AML":
                                 chat_history_1 = {}
     
                                 query = "Is there any Money Laundering activity based on the transaction statements?"
-                                context_1 = docsearch.similarity_search(query, k=5)
+                                context_1 = text_data_doc
                                 prompt_1 = f'''You Are an Anti-Money Laundering Specialist who is an expert in detecting Money-laundering activity. \n
                                 You sholud closely look into the credit card transaction statement as well as savings account transaction statement collectively and evaluate \
                                 them together to check for any potential suspicious money laundering activities. \n
@@ -1945,7 +1945,7 @@ elif selected_option_case_type == "AML":
                 
     
                                 query = "What are the transaction that can be associated with Money Laundering activity?"
-                                context_1 = docsearch.similarity_search(query, k=5)
+                                context_1 = text_data_doc
                                 prompt_1 = f''' You Are an Anti-Money Laundering Specialist and your goal is to detect the Transactions involved in Money laundering activity by taking below considerations:\n\n\
                                 1.) Is There any high cash transactions happening of amount >= 10,000 USD value threshold.\n\n\
                                 2.) If there is a high-value international transaction is happening or If there is any money laundering pattern like structuring or smurfing, layering, placement, integration, etc observed within the credit card and savings bank account transactions statements collectively.\n\n\
