9ca95d5a581368da3a694602310a2efe15faca2a
prince aggarwal <prince183378@exlservice.com>
2023-10-19 17:52:33 +0530

    New commit

--- app2.py
+++ app2.py
@@ -1937,9 +1937,9 @@ elif selected_option_case_type == "AML":
                                 context_1 = text_data_doc
                                 prompt_1 = f''' You Are an Anti-Money Laundering Specialist and your goal is to detect out the Transactions that are involved in the Money laundering activity by taking below considerations:\n\n\
                                 
-                                Are there any Payments of greater than or equal to $10000 made to any unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
+                                Are there any Debited Payments of greater than or equal to $10000 made to any unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
                                 
-                                Based on the above considerations, Output the potential Money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
+                                Based on the above considerations, Output the potential Debited Money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
                                 Response: (Give only the transactions. Do not give me any Explanation,Note, etc.)'''
@@ -1971,10 +1971,10 @@ elif selected_option_case_type == "AML":
 
                                 query = "What is the total amount associated with the money laundering activity?"
                                 context_1 = text_data_doc
-                                prompt_1 =  f'''You Are an Anti-Money Laundering Specialist, give the total amount \
+                                prompt_1 =  f'''You Are an Anti-Money Laundering Specialist, Output the total amount \
                                             associated with money laundering activity that is taking place Based on the \
-                                            transaction statement, for getting the total amount, you can add all the money laundering \
-                                            transactions amount.\n\n\
+                                            Potential Money Laundering Debited transactions, for getting the total amount, you can add all of the  \
+                                            transactions.\n\n\
                                             Context: {context_1}\n\
                                             Question: {query}\n\
                                             Response: (Give me a concise response in one sentence.Do not give me any Explanation,Note)'''
