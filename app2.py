72fc5ded7e5abe6a2db17b141442bca7da4cad34
prince aggarwal <prince183378@exlservice.com>
2023-10-24 13:14:06 +0530

    New commit

--- app2.py
+++ app2.py
@@ -1881,7 +1881,7 @@ elif selected_option_case_type == "AML":
                                                 Provide your recommendation as to if there can be a Money Laundering activity taking place or not based on considering all of the factors above.\n\n\
                                                 Question: {query}\n\
                                                 Context: {context_1}\n\
-                                                Response: '''
+                                                Response: Give a concise response'''
                                 response = usellm(prompt_1)
                                 # query_d="Is there any evidence of unusual activity?"
                                 # query_d=f'**{query_d}**'
