01884b2b183ceef7e4733b7bc21022ad52490ee7
prince aggarwal <prince183378@exlservice.com>
2023-10-25 22:42:33 +0530

    New commit

--- app2.py
+++ app2.py
@@ -1874,14 +1874,16 @@ elif selected_option_case_type == "AML":
                                                 You sholud closely look into the trasactions statements data and evaluate \
                                                 it to check for any potential money laundering activity. \n
                                                 A Money laundering activity can be detected if any of the following transaction patterns is observed :\n
-                                                1) If there are multiple cash deposits of greater than or equals to 10,000 dollars.
-                                                2) If there is any high-value international transaction happening which involves movement of funds to or from a high risk geographical location (Ex- Mauritious, Syria, Nigeria,etc.).
-                                                3) If there is any money laundering pattern like structuring or smurfing, layering, placement, integration, etc observed within 
+                                                1) If there are Multiple cash deposits of greater than or equals to 5000 USD.
+                                                2) If there is any high-value international transaction which involves movement of funds to or from a high risk geographical location (Ex- Mauritious, Syria, Nigeria,etc.).
+                                                3) If there is any money laundering pattern like structuring, smurfing, layering, placement or integration, etc observed within 
                                                 the transactions statements collectively.
-                                                Give your recommendation as to if there can be a Money Laundering activity taking place or not based on considering all of the factors above.\n\n\
+                                                4) Are there any Payments made greater than or equal to 5000 USD to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.)
+                                            
+                                                Give your recommendation as to if there can be a Money Laundering activity going on or not based on considering all above factors.\n\n\
                                                 Question: {query}\n\
                                                 Context: {context_1}\n\
-                                                Response: Give a concise response'''
+                                                Response: Give a concise response of the recommendation'''
                                 response = usellm(prompt_1)
                                 # query_d="Is there any evidence of unusual activity?"
                                 # query_d=f'**{query_d}**'
