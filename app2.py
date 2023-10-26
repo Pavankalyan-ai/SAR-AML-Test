c632d9a6ebcf7f0df42eb77e1d5a0ed72af1632f
prince aggarwal <prince183378@exlservice.com>
2023-10-26 15:59:59 +0530

    New commit

--- app2.py
+++ app2.py
@@ -1893,17 +1893,17 @@ elif selected_option_case_type == "AML":
                                 query = "What are the transaction that can be associated with Money Laundering activity?"
                                 context_1 = text_data_doc
                                 prompt_1 = f''' You Are an Anti-Money Laundering Specialist and your goal is to detect the Transactions that can be involved in Money laundering activity \n\n\
-                                such transactions can be : \n\
-                                # cash deposits of greater than $5,000.\n\n\
-                                # Payment transfers of greater than > $5000 to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
+                                # Money Laundering transactions can be : \n\
+                                1.) Cash deposits of greater than $5,000.\n\n\
+                                2.) Payment transfers of greater than > $5000 to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
                                 
-                                Based on the above considerations , identify potential money laundering transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
+                                Based on the above two considerations , identify potential money laundering transcations.# Print distinct transactions only\n\n\
                                 Also, # do not include any Card due payment Cleared 
                                 # do not include any ATM withdrawals transactions
                                 
                                 Question: {query}\n\
                                 Context: {context_1}\n\
-                                Response: (Additionaly print out the transactions that can also can be related to money laundering regardarless of the defined criteria in a seperate paragraph .# Do not give me any Explanation,Note, etc.)'''
+                                Response: (Give response as transactions only . # Do not give me any Explanation,Note, etc.)'''
                                 response = usellm(prompt_1)
                                 print(response)
 
