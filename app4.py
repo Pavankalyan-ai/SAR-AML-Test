72aca527ede724b61f9bcd5df6ed402920df04d9
prince aggarwal <prince183378@exlservice.com>
2023-11-01 16:22:15 +0530

    New commit

--- app4.py
+++ app4.py
@@ -1932,7 +1932,7 @@ elif selected_option_case_type == "AML":
                                 prompt_1 = f''' Your goal is to extract the Transactions involved in Money laundering activity by taking below considerations:\n\n\
                                 Consideration: Are there any Payments made greater than 5000 to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
                                 Based on the above considerations , identify all the potential money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
-                                # Do not repeat the transactions
+                                # Do not double count the transactions
                                 Question: {query}\n\
                                 Context: {context_1}\n\
                                 Response: (Give me a concise response as transactions only.Do not give me any Explanation,Note, etc.)'''
