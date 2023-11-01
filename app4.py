bb930a537a379bce553ad2aff42b3fd625bbb9d6
prince aggarwal <prince183378@exlservice.com>
2023-11-01 16:35:12 +0530

    New commit

--- app4.py
+++ app4.py
@@ -1940,14 +1940,14 @@ elif selected_option_case_type == "AML":
                                 response = usellm(prompt_1)
                                 prompt_2 = f''' Your goal is to extract the Transactions involved in Money laundering activity by taking below considerations:\n\n\
                                 Consideration: trasactions that are uncommon or unusual and falls between $2000 to $5000 \n\n\
-                                Based on the above considerations , identify all the potential money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
+                                Based on the above considerations , identify all those potential money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
                                 Response: (Give me a concise response as transactions only.Do not give me any Explanation,Note, etc.)'''
                                 response_2 = usellm(prompt_2)
                                 
                                 save_res = response
-                                response3= save_res + response_2
+                                response3= save_res + "There are some other suspicious transactions as below:  " + response_2
                                
                                
                                 chat_history_1[query] = response3
