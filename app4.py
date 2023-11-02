1f098b4d282c449fa41665ac6fbc52252c6079e1
prince aggarwal <prince183378@exlservice.com>
2023-11-02 17:33:41 +0530

    New commit

--- app4.py
+++ app4.py
@@ -1912,12 +1912,16 @@ elif selected_option_case_type == "AML":
                                 context_1 = text_data_doc
                                 prompt_1 = f'''You Are an Anti-Money Laundering Specialist who is an expert in detecting Money-laundering activity. \n
                                                 You sholud closely look into the trasactions statements data and evaluate \
-                                                it to check for any potential money laundering activity. \n\
-                                                Do not highlight or show any transaction which is raising concern.\n\
-                                                Answer below question based on the Context provided .\n\n\
+                                                it to check for any potential money laundering activity. \n
+                                                A Money laundering activity can be detected if any of the following transaction patterns is observed :\n
+                                                1) If there are multiple cash transactions of greater than or equals to $5000.
+                                                2) If there is any high-value international transaction happening which involves movement of funds to or from a high risk geographical location (Ex- Mauritious, Syria, Nigeria,etc.).
+                                                3) If there is any money laundering pattern like structuring or smurfing, layering, placement, integration, etc observed within \ 
+                                                the transactions statements collectively.
+                                                Provide your recommendation as to if there can be a Money Laundering activity taking place or not based on considering all of the factors above.\n\n\
                                                 Question: {query}\n\
                                                 Context: {context_1}\n\
-                                                Response: Give me a concise answer.'''
+                                                Response: Give a concise response'''
                                 response = usellm(prompt_1)
                                 
                                 # query_d="Is there any evidence of unusual activity?"
