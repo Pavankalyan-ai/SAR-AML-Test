a4abe65a20bd0a6039de7c5902e26f7c503fb09a
prince aggarwal <prince183378@exlservice.com>
2023-11-02 13:10:32 +0530

    New commit

--- app4.py
+++ app4.py
@@ -1953,19 +1953,19 @@ elif selected_option_case_type == "AML":
 
                                 query = "What are the transaction that can be associated with Money Laundering activity?"
                                 context_1 = text_data_doc2
-                                prompt_1 = f''' Your goal is to extract all the Transactions that might be involved in Money laundering activity by taking below Consideration:\n\n\
-                                Consideration: Payments greater than $5000 made to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
-                                Identify all the money laundering Debited transcations Based on the above consideration only. Print distinct transactions only\n\n\
+                                prompt_1 = f''' Your goal is to extract out all those Transactions that might be involved in Money laundering activity on the basis of below Consideration:\n\n\
+                                Consideration: Payments that are greater than $5000 made to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
+                                Identify all those money laundering Debited transcations Based on the above consideration only. Print distinct transactions only\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
-                                Response: (Do not give any Explanation,Note, etc. in the answer.)'''
+                                Response: (Do not give or add any Explanation,Note, etc. in the answer.)'''
 
                                 #response2 = usellm(prompt_1)
                                 response = usellm(prompt_1)
                                 save_res = response
                                 #prompt2
                                 prompt_2 = f''' Your goal is to extract all the Transactions that might be involved in Money laundering activity by taking below consideration:\n\n\
-                                Consideration: Debited Transactions of bewteen ($2000 to $5000) amount that can be related to money laundering transactions. \n\n\
+                                Consideration: Debited Transactions of amount bewteen ($2000 to $5000) amount that can be related to money laundering transactions. \n\n\
                                 Based on the above consideration only identify all those Debited transcations. Print distinct transactions only\n\n\
                                 input_data: {context_1}\n\
                                 Response: '''
