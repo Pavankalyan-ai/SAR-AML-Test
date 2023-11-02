04bb0a96d85690ace9f2ce1d947e80edd9e450b3
prince aggarwal <prince183378@exlservice.com>
2023-11-02 13:12:14 +0530

    New commit

--- app4.py
+++ app4.py
@@ -1953,12 +1953,12 @@ elif selected_option_case_type == "AML":
 
                                 query = "What are the transaction that can be associated with Money Laundering activity?"
                                 context_1 = text_data_doc2
-                                prompt_1 = f''' Your goal is to extract out all those Transactions that might be involved in Money laundering activity on the basis of below Consideration:\n\n\
-                                Consideration: Payments that are greater than $5000 made to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
-                                Identify all those money laundering Debited transcations Based on the above consideration only. Print distinct transactions only\n\n\
+                                prompt_1 = f''' Your goal is to extract the Transactions involved in Money laundering activity by taking below consideration:\n\n\
+                                Consideration: Payments greater than $5000 made to an unrecognized entity with no specific business purpose (Ex- Advisories, consultancies,etc.) \n\n\
+                                Based on the above consideration, identify all the potential money laundering debited transcations. Do not double the statemetns from multiple documents, print distinct transactions only\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\
-                                Response: (Do not give or add any Explanation,Note, etc. in the answer.)'''
+                                Response: (Do not give me any Explanation,Note, etc.)'''
 
                                 #response2 = usellm(prompt_1)
                                 response = usellm(prompt_1)
