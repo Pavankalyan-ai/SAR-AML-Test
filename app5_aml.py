c076598250832b516128b97b0e1379b1edfc95ef
prince aggarwal <prince183378@exlservice.com>
2023-11-16 17:09:32 +0530

    New commit

--- app5_aml.py
+++ app5_aml.py
@@ -2149,7 +2149,7 @@ elif selected_option_case_type == "AML":
                                 prompt_1=f''' Your goal is to extract out the suspicious transactions from Credit_Card_statement. Suspicious transactions can be:\n\n
                                 Transactions that are made to an unrecognized entity (Ex- Advisories, consultancies,etc.).#Print distinct transactions only.\n\
                                 Context: {context_1}\n\
-                                Response: (Output those transactions with their Description, Date and Debited Amount. Strictly Do not give/add any Note, Explanation in answer.) '''
+                                Response: (Print those transactions with their Description, Date and Debited Amount.# Strictly Do not give/add any Note, Explanation in answer.) '''
                                 #st.write(context_1)
 
                                 response = usellm(prompt_1)
@@ -2189,7 +2189,7 @@ elif selected_option_case_type == "AML":
                                 prompt_1=f''' Your goal is to pull out suspicious transactions from savings_account_statement. Suspicious transactions can be:\n\n
                                 High Value Cash Deposits in a short span of time. Strictly do not include any Paycheck depostits and Opening balance transaction as they may not be considered as suspicious transactions.
                                 Context: {context_1}\n\
-                                Response: (Output those transaction with their Description, Date and Credited Amount. Also, Do not output any additional text, Explanation or note.) '''
+                                Response: (Print those transaction with their Description, Date and Credited Amount. Also, Do not output any additional text, Explanation or note.) '''
                                 #st.write(context_1)
                                 response = usellm(prompt_1)
 
