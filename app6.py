0f3e264cd35a0f158c94590ff523f6b750b53d2c
prince aggarwal <prince183378@exlservice.com>
2023-11-16 18:58:44 +0530

    New commit

--- app6.py
+++ app6.py
@@ -2146,8 +2146,8 @@ elif selected_option_case_type == "AML":
 
                                 query = "What are the associated suspicious transactions for Credit Card?"
                                 context_1 = docsearch.similarity_search(query, k=5)
-                                prompt_1=f''' Your goal is to print only the suspicious transactions from Credit_Card_statement in Context. Suspicious transactions can be:\n\n
-                                Transactions that are made to an unrecognized entity (Ex- Advisories, consultancies,etc.).Also, do not repeat the same transaction.\n\
+                                prompt_1=f''' Your goal is to print only the suspicious transactions from Credit_Card_statement. Suspicious transactions can be:\n\n
+                                Transactions that are made to an unrecognized entity (Ex- Advisories, consultancies,etc.).strictly do not repeat the same transaction.\n\
                                 Context: {context_1}\n\
                                 Response: (Print ONLY the "Description", "Date" and "Credited" amount of those suspicious transactions.# Strictly do not give/add any further ouput in the answer.) '''
                                 st.write(context_1)
@@ -2297,8 +2297,12 @@ elif selected_option_case_type == "AML":
                                 Context: {contexts}\n\
                                 Response: (Give a concise answer as individual well-formatted points. Also, give your recommendation to the below Question.) 
                                 Question: {query} """
-                                response1 = usellm(prompt)
-                                response1 = response1.replace("$", "USD ")
+                                system_prompt = wrap_prompt("You are a Money Laundering Analyst.", "system")
+                                user_prompt = wrap_prompt(prompt, "user")
+                                res = get_response([system_prompt, user_prompt])
+                                response = res['choices'][0]['message']['content']
+                                #response1 = usellm(prompt)
+                                response1 = response.replace("$", "USD ")
               
 
 
