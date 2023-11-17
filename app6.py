d7fdccd58810efaaf98afc0f1be32f8f295f9bb3
prince aggarwal <prince183378@exlservice.com>
2023-11-17 13:24:39 +0530

    New commit

--- app6.py
+++ app6.py
@@ -2564,10 +2564,11 @@ elif selected_option_case_type == "AML":
                     if selected_option in li_:
                         doc = st.session_state["lineage_aml"][selected_option]
                         for i in range(len(doc)):
-                            st.write(f":blue[Chunk-{i}:]")
-                            st_ = doc[i].page_content.replace("()", " ")
-                            st.write(":blue[Page Content:]",st_) 
-                            st.write(":blue[Source:]",doc[i].metadata['source'])
+                            st.write(doc)
+                            # st.write(f":blue[Chunk-{i}:]")
+                            # st_ = doc[i].page_content.replace("()", " ")
+                            # st.write(":blue[Page Content:]",st_) 
+                            # st.write(":blue[Source:]",doc[i].metadata['source'])
                               
 
   
