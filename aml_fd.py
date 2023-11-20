bd855fa955956a7bcd8fc3c6dd878ae1042f9666
prince aggarwal <prince183378@exlservice.com>
2023-11-20 23:10:08 +0530

    New commit

--- aml_fd.py
+++ aml_fd.py
@@ -2206,7 +2206,7 @@ elif selected_option_case_type == "AML":
                             texts = text_to_docs(json2,file)
                             for i in texts:
                                 temp_file_path2.append(i)
-                st.write(temp_file_path2)            
+               # st.write(temp_file_path2)            
 
     
                 #combining files in fetch evidence and upload evidence
@@ -2319,7 +2319,7 @@ elif selected_option_case_type == "AML":
                                 ## Question-1
 
                                 
-                                st.write(doc_1)
+                                #st.write(doc_1)
     
                                 query = "Why was the transaction triggered?"
                                 
