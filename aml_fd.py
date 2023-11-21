7567efc2f6597a52f7dedae922efdc2c0d5a04d7
prince aggarwal <prince183378@exlservice.com>
2023-11-21 17:31:12 +0530

    New commit

--- aml_fd.py
+++ aml_fd.py
@@ -2818,6 +2818,7 @@ elif selected_option_case_type == "AML":
                 
             with col3_up:
                 if st.session_state["lineage_aml"] is not None:
+                    st.session_state["lineage_aml"]=st.session_state["lineage_aml"].replace("($)", " ")
  
                     li = ["Select question to get the lineage",
                         "Why was the transaction triggered?",
@@ -2841,7 +2842,7 @@ elif selected_option_case_type == "AML":
                             #st.write(doc[i])
                             y=i+1
                             st.write(f":blue[Chunk-{y}:]")
-                            st_ = doc[i].page_content.replace("($)"," ")
+                            st_ = doc[i].page_content#.replace("($)"," ")
                             st.write(":blue[Page Content:]",st_) 
                             st.write(":blue[Source:]",doc[i].metadata['source'])
                               
