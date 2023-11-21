f9759b8d356162c02f53b94a7e83f9466b409498
prince aggarwal <prince183378@exlservice.com>
2023-11-21 17:23:59 +0530

    New commit

--- aml_fd.py
+++ aml_fd.py
@@ -2835,19 +2835,15 @@ elif selected_option_case_type == "AML":
 
         
                         doc = st.session_state["lineage_aml"][selected_option]
-                        doc_new=[]
-                        for i in doc:
-                            z=i.page_content.replace("$"," ")
-                            doc_new.append(z)
-                        st.write(doc_new)
                         
-                        for i in range(len(doc_new)):
+                        
+                        for i in range(len(doc)):
                             #st.write(doc[i])
                             y=i+1
                             st.write(f":blue[Chunk-{y}:]")
-                            st_ = doc_new[i].page_content
+                            st_ = doc[i].page_content.replace("($)"," ")
                             st.write(":blue[Page Content:]",st_) 
-                            st.write(":blue[Source:]",doc_new[i].metadata['source'])
+                            st.write(":blue[Source:]",doc[i].metadata['source'])
                               
 
   
