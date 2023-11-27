779e282774abf6e6d295cb6a5b012904d4abbc0b
prince aggarwal <prince183378@exlservice.com>
2023-11-27 14:25:15 +0530

    New commit

--- aml_fd_v2.py
+++ aml_fd_v2.py
@@ -2507,13 +2507,13 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query  = "Give your recommendation if this is a Suspicious activity or not?"
                                 contexts = ques1 + ques5
-                                prompt_2 = f"""You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only and if it SAR fillins is required or not ?,\n\n\
+                                prompt_2 = f"""Add a one line introductory statement on if sar filling is required based on below questions or not. Based on the given context, Give concise response to the each question below.\n\n\
                                 1.) transaction triggered\n\
                                 2.) amounts related to money laundering for savings account and credit cards\n\
                                 3.) Type of money laundering activity taking place and why ?\n\     
                                 Based on above points, give your recommendation if this is a case of suspicious activity or not? \n\n\                     
                                 Context: {contexts}\n\
-                                Response: (Give me a concise recommendation in few pointers.If not a case of suspicion them mention it properly.) 
+                                Response: (Give a neatly formatted response for each question individually.) 
                                 """
                                 system_prompt = wrap_prompt("You are a Money Laundering Analyst.", "system")
                                 user_prompt = wrap_prompt(prompt_2, "user")
