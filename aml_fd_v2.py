9103b69298cfb282bf1e0d2949ab622bea560d07
prince aggarwal <prince183378@exlservice.com>
2023-11-27 14:21:00 +0530

    New commit

--- aml_fd_v2.py
+++ aml_fd_v2.py
@@ -2507,7 +2507,7 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query  = "Give your recommendation if this is a Suspicious activity or not?"
                                 contexts = ques1 + ques5
-                                prompt_2 = f"""You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
+                                prompt_2 = f"""You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only and if it SAR fillins is required or not ?,\n\n\
                                 1.) transaction triggered\n\
                                 2.) amounts related to money laundering for savings account and credit cards\n\
                                 3.) Type of money laundering activity taking place and why ?\n\     
