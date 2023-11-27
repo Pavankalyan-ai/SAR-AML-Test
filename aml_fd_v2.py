a007a3c64aabee39fa68b947f70fad8a0e4f0735
prince aggarwal <prince183378@exlservice.com>
2023-11-27 12:20:18 +0530

    New commit

--- aml_fd_v2.py
+++ aml_fd_v2.py
@@ -2507,7 +2507,7 @@ elif selected_option_case_type == "Money Laundering":
 
                                 query  = "Give your recommendation if this is a Suspicious activity or not?"
                                 contexts = ', '.join(res_df_gpt['Answer'])
-                                prompt_2 = f"""Start the answer with a one liner and Give concise response to the each questions below within the given Context \n\
+                                prompt_2 = f"""Give concise response to the each questions below within the given Context \n\
                                 1.) transaction triggered\n\
                                 2.) amounts related to money laundering for savings account and credit cards \n\
                                 3.) type of money laundering activity taking place and why.\n\                          
