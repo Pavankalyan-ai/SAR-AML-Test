e8af1ccc2be5d9713e39d6a04b67fc503ad267e6
prince aggarwal <prince183378@exlservice.com>
2023-12-01 16:39:15 +0530

    ne commit

--- aml_fd_v2.py
+++ aml_fd_v2.py
@@ -2916,9 +2916,9 @@ elif selected_option_case_type == "Money Laundering":
                             # st.session_state["tmp_summary_gpt_aml"]=st.session_state["tmp_summary_gpt_aml"].replace("$", "USD ")
 
                             ## using open ai:
-                            st.write(summary1)
+                            #st.write(summary1)
 
-                            prompt_summ=f'''Provide a detailed summary of the below Context and make sure to include all the relevant information (this includes names, transactions, involved parties, amounts involved, etc). Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
+                            prompt_summ=f'''Provide a concise summary of the below Context and make sure to include all the relevant information (this includes names, transactions, involved parties, amounts involved, etc). Provide the summary in a single paragraph and don't include words like these: 'chat summary', 'includes information' or 'AI' in my final summary.\n\n\
                             Context: {summary1}  '''
                             system_prompt = wrap_prompt("You are a summarization tool", "system")
                             user_prompt = wrap_prompt(prompt_summ, "user")
